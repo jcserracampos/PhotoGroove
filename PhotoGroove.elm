911c56f208732e5c475339c12c23118b33a02cd8
Júlio Campos <jcserracampos@gmail.com>
2018-07-27 11:04:13 -0300

    My first view

---
+++ PhotoGroove.elm
@@ -0,0 +1,15 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+view model =
+    div [class "content"]
+        [ h1 [] [ text "Photo Groove"]
+        , div [ id "thumbmails"]
+            [ img [ src "http://elm-in-action.com/1.jpeg" ] [] 
+            , img [ src "http://elm-in-action.com/2.jpeg" ] []
+            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+        ]
+    ]
+
+main =
+    view "no model yet"
