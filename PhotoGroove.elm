4748ef37d77b404b7ba193535d3a22721fb8b962
Júlio Campos <julio.campos@al.go.leg.br>
2018-08-07 14:33:42 -0300

    add a Http request

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -5,6 +5,7 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Array exposing (Array)
 import Random
+import Http
 
 urlPrefix : String
 urlPrefix =
@@ -25,15 +26,23 @@ view model =
             (List.map viewSizeChooser [ Small, Medium, Large ] )
         , div [ id "thumbnails", class (sizeToClass model.chosenSize) ] 
             (List.map (viewThumbnail model.selectedUrl) model.photos )
-            , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ] []
+            , viewLarge model.selectedUrl
     ]
 
-viewThumbnail : String -> Photo -> Html Msg
+viewLarge : Maybe String -> Html Msg
+viewLarge maybeUrl =
+    case maybeUrl of
+        Nothing ->
+            text ""
+        Just url ->
+            img [ class "large", src (urlPrefix ++ "large/" ++ url)] []    
+
+viewThumbnail : Maybe String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumbnail = 
         img [ src (urlPrefix ++ thumbnail.url)
         -- Html.classList builds a class attribute using a list of touples
         -- first comes the desired class and second a bolean for whether to include
-        , classList [ ( "selected", selectedUrl == thumbnail.url) ], onClick (SelectByUrl thumbnail.url ) ] []
+        , classList [ ( "selected", selectedUrl == Just thumbnail.url) ], onClick (SelectByUrl thumbnail.url ) ] []
 
 -- TODO: 
 -- Start radio checked by model
@@ -69,60 +78,82 @@ type alias Photo =
     { url: String}
 
 type alias Model =
-    { photos: List Photo
-    , selectedUrl: String
-    , chosenSize: ThumbnailSize }
+    { photos : List Photo
+    , selectedUrl : Maybe String
+    , loadingError : Maybe String
+    , chosenSize : ThumbnailSize }
 
 initialModel : Model
 initialModel =
-    { photos = 
-        [ { url = "1.jpeg" } 
-        , { url = "2.jpeg" } 
-        , { url = "3.jpeg" } 
-        ]
-    , selectedUrl = "1.jpeg"
+    { photos = []
+    , selectedUrl = Nothing
+    , loadingError = Nothing
     , chosenSize = Large
     }
-
-photoArray : Array Photo
-photoArray =
-    Array.fromList initialModel.photos
-
-getPhotoUrl : Int -> String
-getPhotoUrl index =
-    case Array.get index photoArray of
-        Just photo ->
-            photo.url
-        Nothing ->
-            ""
-
-randomPhotoPicker : Random.Generator Int
-randomPhotoPicker =
-    Random.int 0 (Array.length photoArray - 1)
 
 type Msg 
     = SelectByUrl String
     | SelectByIndex Int
     | SurpriseMe
     | SetSize ThumbnailSize
+    | LoadPhotos (Result Http.Error String)
 
 update : Msg -> Model -> ( Model, Cmd Msg)
 update message model =
     case message of
         SelectByUrl url ->
-            ( { model | selectedUrl = url }, Cmd.none )
+            ( { model | selectedUrl = Just url }, Cmd.none )
         SelectByIndex index ->
-            ( { model | selectedUrl = getPhotoUrl index}, Cmd.none )
+            let
+                newSelectedUrl : Maybe String
+                newSelectedUrl = 
+                    model.photos
+                        |> Array.fromList
+                        |> Array.get index
+                        |> Maybe.map .url
+            in
+               ( { model | selectedUrl = newSelectedUrl}, Cmd.none ) 
         SurpriseMe ->
-            ( model, Random.generate SelectByIndex randomPhotoPicker )
+            let
+                randomPhotoPicker =
+                    Random.int 0 (List.length model.photos - 1)    
+            in
+                ( model, Random.generate SelectByIndex randomPhotoPicker )
         SetSize size ->
             ( { model | chosenSize = size}, Cmd.none )
+        LoadPhotos (Ok responseStr) ->
+            let
+                urls = 
+                    String.split "," responseStr
+                photos =
+                    List.map Photo urls
+            in
+                    ( { model | photos = photos, selectedUrl = List.head urls }, Cmd.none )
+        LoadPhotos (Err _) ->
+            ( { model | loadingError = Just "Error! (Try turning it off and on again?)"}, Cmd.none )
+
+initialCmd : Cmd Msg
+initialCmd =
+    "http://elm-in-action.com/photos/list"
+        |> Http.getString
+        |> Http.send LoadPhotos
+
+viewOrError : Model -> Html Msg
+viewOrError model =
+    case model.loadingError of
+        Nothing ->
+            view model
+        Just errorMessage ->
+            div [ class "error-message" ]
+                [ h1 [] [ text "Photo Groove" ]
+                , p [] [ text errorMessage]
+                ]
 
 main: Program Never Model Msg
 main =
     Html.program
-        { init = ( initialModel, Cmd.none )
-        , view = view
+        { init = ( initialModel, initialCmd )
+        , view = viewOrError
         , update = update
-        , subscriptions = (\model -> Sub.none)
+        , subscriptions = (\_ -> Sub.none)
         }
